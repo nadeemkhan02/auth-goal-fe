3ac3c294b4f58b9332f0ac8e8c2052d0da3b30a5
navid7715 <navidkhan7715@gmail.com>
2023-09-24 01:22:52 +0530

    update signUp page

--- src/page/SignUp/index.js
+++ src/page/SignUp/index.js
@@ -1,9 +1,47 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 const SignUp = (props) => {
   return (
-    <div>
-      <h1>Sign Up page</h1>
+    <div className="container mt-5 signup-container ">
+      <h1 className="mb-4">Sign Up</h1>
+      <form className="signup-form">
+        <div className="name form-group form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="name" id="name" className="form-control" name="name" />
+        </div>
+        <div className="email form-group form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            className="form-control"
+            name="email"
+          />
+        </div>
+        <div className="password form-group form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            className="form-control"
+            name="password"
+          />
+        </div>
+        <div className="submit form-group form-group">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={(e) => {
+              e.preventDefault();
+              console.log("submitted");
+            }}
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
